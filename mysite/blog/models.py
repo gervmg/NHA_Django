c283238ba16219e05f780ff1f4931989e3e39c08
gervmg <gvaessen@hetnet.nl>
2023-07-16 09:32:51 +0200

    les5

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -19,17 +19,3 @@ class Post(models.Model):
         return self.title
 
 
-class Voetbalspeler(models.Model):    
-    naam_speler = models.CharField(max_length=200)
-    club = models.CharField(max_length=100)
-    naam_auteur = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    datum_invoer = models.DateTimeField(default=timezone.now)
-    datum_aanpassing = models.DateTimeField(blank=True, null=True)
-
-    def publish(self):
-        self.datum_aanpassing = timezone.now()
-        self.save()
-
-    def __str__(self):
-        return self.naam_speler + " speelt bij " + self.club
- 
