607162a82392d78082c95bc3dcb327411291b03c
gervmg <gvaessen@hetnet.nl>
2023-07-22 02:00:13 +0200

    stijl

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [    
-    path('', views.post_list, name='post_list'),
+   # path('', views.testing, name='test'),
+   path('', views.post_list, name='post_list'),
 ]
 
 
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+from django.http import HttpResponse
+from django.template import loader
 
-def post_list(request):    
-    return render(request, 'blog/post_list.html', {})
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def testing(request):
+  template = loader.get_template('blog/test.html')
+  context = {
+    'fruits': ['Apple', 'Banana', 'Cherry'],   
+  }
+  return HttpResponse(template.render(context, request))            
