57e9dfe3261d8af4b661b2ebe97c9b03e5b35afb
gervmg <gvaessen@hetnet.nl>
2023-07-27 20:02:26 +0200

    poll exit

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -5,5 +5,5 @@ from django.conf.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
-    path('polls/', include('polls.urls')),
+    # path('polls/', include('polls.urls')),
 ]
