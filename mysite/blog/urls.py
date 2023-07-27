e2956fbf0ca6bf7ffb56de9a92b03741d6688b20
gervmg <gvaessen@hetnet.nl>
2023-07-27 18:40:56 +0200

    poll

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [    
    # path('', views.testing, name='test'),
    path('', views.post_list, name='post_list'),
+   path('post/<int:pk>/', views.post_detail, name='post_inhoud'),
+   path('post/nieuw/', views.post_nieuw, name='post_nieuw'),
 ]
 
 
